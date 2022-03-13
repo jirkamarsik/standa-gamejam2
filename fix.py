3f3c5ceda900045f893a4e9a9e5d1caf4cb72381
Jirka Marsik <jiri.marsik@oracle.com>
2022-03-13 16:28:00 +0100

    Generalize fix.py

--- fix.py
+++ fix.py
@@ -3,13 +3,20 @@
 import re
 import sys
 
-src = open('Karty - List 1.csv', 'r').read()
+in_file = sys.argv[1] if len(sys.argv) >= 2 else 'Karty - List 1.csv'
+out_file = re.sub(r'(.*)\.csv', r'\1 - fixed.csv', in_file)
 
-src = re.sub(r'(?<!\r)\n', '<br>', src)
+# Read
+csv = open(in_file, 'r', newline='').read()
 
+# Fix newlines
+csv = re.sub(r'[^\r]\n', r'<br>', csv)
+
+# Fix non-ASCII chars
 def use_entity(match):
     return "&#" + str(ord(match.group(0)[0])) + ";"
 
-src = re.sub(r'[\x80-\u10FFFF]', use_entity, src)
+csv = re.sub(r'[\x80-\u10FFFF]', use_entity, csv)
 
-print(src, file=open('Karty-fixed.csv', 'w'), end='')
+# Write
+print(csv, file=open(out_file, 'w'), end='')
